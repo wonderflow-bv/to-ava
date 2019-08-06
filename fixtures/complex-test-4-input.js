c8611f02b3b9959856171423f9bbf050cdbf5fae
bitliner <giovanni.gaglione@gmail.com>
2019-08-06 22:56:35 +0200

    fixed case in which real value is not a variable, but a property of an object

---
+++ fixtures/complex-test-4-input.js
@@ -0,0 +1,8 @@
+/* eslint-env jest */
+
+describe('test 1', () => {
+  it('should work find', () => {
+    const res = 2 + 3
+    expect(res.length).toEqual(5)
+  })
+})
---
+++ fixtures/complex-test-4-output.js
@@ -0,0 +1,4 @@
+test('test 1 > should work find', (t) => {
+  const res = 2 + 3
+  t.deepEqual(res.length, 5)
+})
