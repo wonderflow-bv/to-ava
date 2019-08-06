44e754096b38dbdb96ff92cd5a2e626195c1bf3b
bitliner <giovanni.gaglione@gmail.com>
2019-08-06 22:56:55 +0200

    fixed cli

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { omit } = require('lodash')
 const { resolve } = require('path')
 
 module.exports = args => {
+  console.log(args)
   console.log(chalk.green(chalk.bold(`Transforming using '${args.transform}' ✌`)))
   const options = Object.assign({ quote: 'single' }, omit(args, ['_', 'transform', 'path']))
   Runner.run(resolve(__dirname, `lib/to-ava-transformer.js`), args.path, options)
