383226b8891c498511ec6ff0ab7b58a1a720bd5d
bitliner <giovanni.gaglione@gmail.com>
2019-08-17 12:47:56 +0200

    added support for test.each() - transformed into macros

--- fixtures/complex-test-11-with-each-method-output.spec.js
+++ fixtures/complex-test-11-with-each-method-output.spec.js
@@ -1,14 +1,14 @@
 const { serial: test } = require('ava')
 
-function macro1 (t, input, expected) {
-  t.deepEqual(input * 2, expected)
-}
-
 let outsideVar;
 
 test.beforeEach(() => {
   outsideVar = 2
 })
 
-test('test 1 > should work find 1/2', macro1, 2, 4)
-test('test 1 > should work find 2/2', macro1, 3, 6)
+test('test 1 > should return the double 2/2', macro0, 3, 6)
+test('test 1 > should return the double 1/2', macro0, 2, 4)
+
+function macro0 (t, input, expected) {
+  t.deepEqual(input * 2, expected)
+}
